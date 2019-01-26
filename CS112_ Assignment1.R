2ca4262e951799b5d3a2d01608878d7a1936db1f
danyalnaeem <40008875+danyalnaeem@users.noreply.github.com>
2019-01-25 16:45:37 -0800

    Update CS112_ Assignment1.R

--- CS112_ Assignment1.R
+++ CS112_ Assignment1.R
@@ -25,6 +25,7 @@ new_foo <- foo[-which.have.NAs, ] #Eliminates the rows with NA in CirculationDat
 x <- (which(new_foo$CirculationDate >= '2008-01-01')) #identifies which rows in CirculationDate are greater than equal to 2008-01-01
 over_Jan1<- new_foo[x,] #Includes circulation date greater or equal to Jan 1,2008 only
 
+#Question 1a
 duration <- over_Jan1$OriginalCompletionDate - over_Jan1$ApprovalDate #calculates the purpoted duration 
 mean(duration, na.rm=TRUE) #calculates mean for purpoted duration and sets NA = TRUE so that it is not included in calculation 
 median(duration, na.rm=TRUE)#calculates median for purpoted duration and sets NA = TRUE so that it is not included in the calculation
@@ -32,6 +33,9 @@ Q1<- quantile(duration, 0.25, na.rm=TRUE) #calculates 25th percentile for purpot
 Q3<- quantile(duration, 0.75, na.rm= TRUE)#calculates 75th percentile for purpoted duration and sets NA = TRUE so that it is not included
 IQR<- Q3-Q1 #calculates interquartile range by subtracting 25th percentile from 75th
 IQR
+hist(as.numeric(((duration)/365)*12), xlab = "Purpoted Duration (in months)") #generates a histogram for the duration
+
+#Question 1b
 #Lines 36 to 42 do the same thing as in lines 28 to 34 except that this mean, median and IQR is for the actual duration
 actual_duration <- over_Jan1$RevisedCompletionDate - over_Jan1$ApprovalDate
 mean(actual_duration)
@@ -40,7 +44,9 @@ Q1<- quantile(actual_duration, 0.25, na.rm=TRUE)
 Q3<- quantile(actu1al_duration,0.75,na.rm = TRUE)
 IQR_actual <-Q3-Q1
 IQR_actual
+hist(as.numeric(((actual_duration)/365)*12), xlab = "Actual Duration (in months)") #generates a histogram for the duration
 
+#Question 2
 which.have.NAs <- which(is.na(over_Jan1$Rating == TRUE)) #this identifies rows that have NA in the rating column
 new_ratings <- over_Jan1[-which.have.NAs, ] #eliminates rows with NA in Rating column
 rating0<- new_ratings[ which(new_ratings$Rating=='0'),] #identifies the rows with rating = 0
@@ -57,6 +63,7 @@ print(ratingone)
 print(ratingtwo)
 print(ratingthree)
 
+#Question 3
 which.have.PPTAs <- which(over_Jan1$Type== "PPTA")#this identifies which projects have the type = PPTA
 no_PPTA <- over_Jan1[-which.have.PPTAs, ] #Eliminates projects with type = PPTA
 rating0<- no_PPTA[ which(no_PPTA$Rating=='0'),] #identifies the rows with rating = 0
@@ -73,6 +80,7 @@ print(ratingone)
 print(ratingtwo)
 print(ratingthree)
 
+#Question4
 sorted <- over_Jan1[order(over_Jan1$RevisedAmount),] #Sorts the data in ascending order based on the RevisedAmount
 bottom25_<-sorted[1:(0.25*nrow(sorted)),] #Filters out the least 25 amount of budgets 
 top25_ <- sorted[(nrow(sorted)-(0.75*nrow(sorted))):(nrow(sorted)),] #Filters out the top 25 amount of budgets
